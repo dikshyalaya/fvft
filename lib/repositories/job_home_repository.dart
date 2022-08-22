1b338cc0c99ec6caf40133960f03b1169fff0010
Rabin Acharya <charms2074@gmail.com>
2022-08-22 11:50:46 +0545

    created new job section

--- lib/repositories/job_home_repository.dart
+++ lib/repositories/job_home_repository.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:free_visa_free_ticket/core/network/api_manager.dart';
 
+
+
+// h
 class JobHomeRepository{
  static final ApiManager   _apiManager = ApiManager();
 static const String _jobHomeEndPoint = '/v1/candidate/job/home';
